5e182ba3ab8d0b866e32d701cf07d62d055978f9
chxxing <choing26@naver.com>
2022-11-01 11:49:47 +0900

    221101

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import List from './List';
+import './common.scss';
+import { Link, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+    const genreList = [
+        "Action",
+        "Adventure",
+        "Animation",
+        "Comedy",
+        "Crime",
+        "Drama",
+        "Fantasy",
+        "Romance",
+        "Thriller",
+        "Western"
+    ];
     return (
         <div>
-            App
+            <ul className='List'>
+                {
+                    genreList.map(it => {
+                        return (
+                            <li>
+                                <Link to={it}>{it}</Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <Routes>
+                {
+                    genreList.map(it => {
+                        return (
+                            <Route path={it} element={<List genre={it} limit={5} />} />
+                        )
+                    })
+                }
+            </Routes>
+
+            <List genre='Drama' limit={20} />
+            <List genre='Action' limit={20} />
+            <List genre='Horror' limit={20} />
         </div>
     )
 }
